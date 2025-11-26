aaa398bfb06d5ed33fff2c44f76d2d8b1848507d
Hasib <gudhulibela031@gmail.com>
2025-11-26 20:59:33 +0600

    Add files via upload

---
+++ 16.Find the LCM of two numbers.cpp
@@ -0,0 +1,17 @@
+//16.Write a program to find the LCM of two numbers.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,r;
+    cout<<"Enter the numbers: ";
+    cin>>a>>b;
+int num1=a,num2=b;
+    while(b!=0)
+    {
+        r=a%b;
+        a=b;
+        b=r;
+    }
+    cout<<"The LCM of "<<num1<<","<<num2<<" is "<<(num1*num2)/a;
+}
